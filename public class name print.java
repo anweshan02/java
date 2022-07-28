4937284254eafca38974b3b5842212222cd32363
anweshan02 <109446395+anweshan02@users.noreply.github.com>
2022-07-28 08:02:33 +0545

    Add files via upload

---
+++ public class name print.java
@@ -0,0 +1,11 @@
+public class name print
+{
+    public static void main (string[]args)
+    {
+        for(int i=1; i<=10; i++)
+        {
+            system.out.printf("Anweshan");
+            
+        }
+    }
+}
